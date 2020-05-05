c8c27a4565a439e0f247dbd3f2298cae82812768
Jakub Žitný <jakubzitny@avocode.com>
2020-05-05 21:54:26 +0200

    extract service factories to a separate file

--- src/index.js
+++ src/index.js
@@ -1,49 +1,8 @@
 // @flow
 
-import fs from 'fs';
-import os from 'os';
-import readline from 'readline';
-
-import got from 'got';
-import yargs from 'yargs';
-
-import Interviewer from './interviewer';
-import OpenWeatherMapApiRequestor, {
-  validateApiData
-} from './api/openweathermap-api-requestor';
-import ApiRequestor from './api/api-requestor';
-import CliParser from './cli-parser';
-import { detectLocation } from './location-detector';
+import { initServices } from './service-factory';
+import { validateApiData } from './api/openweathermap-api-requestor';
 import { convertScale } from './utils/scale-converter';
-
-const initServices = () => {
-  const interviewerServices = {
-    readline,
-    process // NOTE: Node global
-  };
-
-  const interviewer = new Interviewer(interviewerServices);
-  const apiRequestor = new ApiRequestor({ got });
-  const openWeatherMapApiRequestor = new OpenWeatherMapApiRequestor({
-    apiRequestor,
-    process
-  });
-
-  const cliParserServices = {
-    fs,
-    interviewer,
-    os,
-    yargs
-  };
-
-  const cliParser = new CliParser(cliParserServices);
-
-  return {
-    cliParser,
-    interviewer,
-    openWeatherMapApiRequestor
-  };
-};
 
 const main = async () => {
   const services = initServices();
---
+++ src/service-factory.js
@@ -0,0 +1,47 @@
+// @flow
+
+import fs from 'fs';
+import os from 'os';
+import readline from 'readline';
+
+import got from 'got';
+import yargs from 'yargs';
+
+import Interviewer from './interviewer';
+import OpenWeatherMapApiRequestor from './api/openweathermap-api-requestor';
+import IpApiRequestor from './api/ip-api-requestor';
+import ApiRequestor from './api/api-requestor';
+import CliParser from './cli-parser';
+import LocationDetector from './location-detector';
+
+export function initServices() {
+  const interviewerServices = {
+    readline,
+    process // NOTE: Node global
+  };
+
+  const interviewer = new Interviewer(interviewerServices);
+  const apiRequestor = new ApiRequestor({ got });
+  const ipApiRequestor = new IpApiRequestor({ apiRequestor });
+  const openWeatherMapApiRequestor = new OpenWeatherMapApiRequestor({
+    apiRequestor,
+    process
+  });
+
+  const locationDetector = new LocationDetector({ ipApiRequestor });
+  const cliParserServices = {
+    fs,
+    interviewer,
+    locationDetector,
+    os,
+    yargs
+  };
+
+  const cliParser = new CliParser(cliParserServices);
+
+  return {
+    cliParser,
+    interviewer,
+    openWeatherMapApiRequestor
+  };
+}
